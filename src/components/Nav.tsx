744f7eae0ceab2a0af0e11ec7886b2f13aa5d745
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-11 16:25:34 -0500

    feat: swap menu order
    i like this order better

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,8 +4,8 @@ function Nav() {
   return (
     <div style={{marginBottom: "1em"}}>
       <Link className="button" to="/">Home</Link>
-      <Link className="button" to="/events">Events</Link>
       <Link className="button" to="/cats">Cats</Link>
+      <Link className="button" to="/events">Events</Link>
     </div>
   )
 }
