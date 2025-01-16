6fc6ab3bee018d98fa2b1c392a0a79c078dae139
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-16 17:56:42 -0500

    chore: breadcrumbs component

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router";
+
+type Breadcrumb = {
+  url: string;
+  label: string;
+};
+
+function Breadcrumbs({ crumbs }: { crumbs: Breadcrumb[] }) {
+  return (
+    <div>
+      {crumbs.map((crumb, index) => {
+        const separator = crumbs.length - 1 === index ? "" : " / ";
+        if (index === crumbs.length - 1) {
+          return <span key={index}>{crumb.label}</span>;
+        }
+        return (
+          <span key={index}>
+            <Link to={crumb.url}>{crumb.label}</Link>
+            {separator}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Breadcrumbs;
