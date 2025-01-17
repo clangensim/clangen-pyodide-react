1e361bce936c22671a85420c03604e50b5497f9f
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-17 16:27:26 -0500

    feat: no underline on buttons

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import "./styles/base.css";
 import "../vendors/classic-stylesheets-win9x-2.0/win9x/theme.min.css";
 import "../vendors/classic-stylesheets-win9x-2.0/win9x/skins/teal-2000.css";
+import "./styles/base.css";
 
 import App from "./App.tsx";
 import EventsPage from "./pages/EventsPage.tsx";
