ca8bc9846cb296a16af2bc7a348f0459d10f183a
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-18 21:48:41 -0500

    feat: change name of output
    i don't want people to think they have to unzip it...

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 const download = (b: Blob) => {
   const temp = document.createElement("a");
   temp.href = window.URL.createObjectURL(b);
-  temp.download = "saves.zip";
+  temp.download = "export-data.sav";
   document.body.appendChild(temp);
   temp.click();
   document.body.removeChild(temp);
