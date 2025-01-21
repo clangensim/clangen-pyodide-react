d0d43f95507ebf50b13630a7713b532071dc5645
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-21 17:09:16 -0500

    fix: build works

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import react from '@vitejs/plugin-react'
+
+const PYODIDE_EXCLUDE = [
+  "!**/*.{md,html}",
+  "!**/*.d.ts",
+  "!**/*.whl",
+  "!**/node_modules",
+];
+
+export function viteStaticCopyPyodide() {
+  const pyodideDir = dirname(fileURLToPath(import.meta.resolve("pyodide")));
+  return viteStaticCopy({
+    targets: [
+      {
+        src: [join(pyodideDir, "*")].concat(PYODIDE_EXCLUDE),
+        dest: "assets",
+      },
+    ],
+  });
+}
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopyPyodide(),
+  ],
   optimizeDeps: {
     exclude: [
       "pyodide",
