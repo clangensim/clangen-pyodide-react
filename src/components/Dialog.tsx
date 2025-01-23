463326d60436515bd3e8ec51f35546770ee89b29
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-23 18:06:46 -0500

    chore: dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+
+function Dialog({
+  title,
+  opened,
+  onClose = () => {},
+  children,
+}: {
+  title: string;
+  opened: boolean;
+  onClose?: () => void;
+  children?: React.ReactNode;
+}) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  if (opened) {
+    if (dialogRef.current && !dialogRef.current.open) {
+      dialogRef.current.showModal();
+    }
+  } else {
+    if (dialogRef.current && dialogRef.current.open) {
+      dialogRef.current.close();
+    }
+  }
+
+  function handleClose() {
+    onClose();
+  }
+
+  return (
+    <>
+      <dialog ref={dialogRef} style={{ minWidth: 250 }}>
+        <div className="window">
+          <div className="title-bar active">
+            <div className="title-bar-text">{title}</div>
+            <div className="title-bar-buttons">
+              <button data-close="" onClick={handleClose} />
+            </div>
+          </div>
+          <div className="window-body">{children}</div>
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+export default Dialog;
