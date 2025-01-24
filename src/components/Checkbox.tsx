1663e6170f375795c80838af219ddb6a5a8ddeb6
Lara <63805048+larkgz@users.noreply.github.com>
2025-01-24 13:33:16 -0500

    chore: checkbox component

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,24 @@
+import { useId } from "react";
+
+function Checkbox({
+  label,
+  checked,
+  onChange,
+}: {
+  label: string;
+  checked?: boolean;
+  onChange?: () => void;
+}) {
+  const ID = useId();
+
+  return (
+    <>
+      <div>
+        <input id={ID} checked={checked} onChange={onChange} type="checkbox" />
+        <label htmlFor={ID}>{label}</label>
+      </div>
+    </>
+  );
+}
+
+export default Checkbox;
