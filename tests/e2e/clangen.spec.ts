c22f11e43ec480aca83674af1f9976be3a0e7011
Lara <63805048+larkgz@users.noreply.github.com>
2025-05-12 16:53:13 -0400

    test: add test for signup

---
+++ tests/e2e/clangen.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+
+test('signup', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+
+  // Redirect to signup page when no account
+  // Also confirms that clangen loaded successfully
+  await expect(page).toHaveURL(/signup/);
+  await expect(page).toHaveTitle("Welcome! | ClanGen Simulator");
+
+  // entered create new clan page
+  await page.getByRole("link", { name: "Create New Clan" }).click();
+  await expect(page).toHaveTitle("New Clan | ClanGen Simulator");
+
+  // sign up
+  await page.locator("#clan-name-input").fill("Test");
+
+  // select cats
+  const leader = page.locator("[name='leader']");
+  const deputy = page.locator("[name='deputy']");
+  const med = page.locator("[name='med']");
+
+  await leader.selectOption({ index: 1 });
+  await deputy.selectOption({ index: 1 });
+  await med.selectOption({ index: 1 });
+
+  const leaderName: string = await leader.evaluate("el => el.options[el.selectedIndex].text");
+  const deputyName: string = await deputy.evaluate("el => el.options[el.selectedIndex].text");
+  const medName: string = await med.evaluate("el => el.options[el.selectedIndex].text");
+
+  // submit
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page).toHaveURL("http://localhost:5173/cats");
+
+  const profileInfo = page.locator(".profile-info").first();
+  await profileInfo.waitFor();
+
+  // loaded ok?
+  await expect(profileInfo).toHaveText(/TestClan/);
+  await expect(profileInfo).toHaveText(/Newleaf/);
+  await expect(profileInfo).toHaveText(/0 moons/);
+
+  // selected cats ok?
+  const catsList = page.locator(".cats-list").first();
+  await expect(catsList).toHaveText(RegExp(deputyName));
+  await expect(catsList).toHaveText(RegExp(medName));
+
+  // check that it saved
+  await page.reload();
+  await expect(profileInfo).toHaveText(/TestClan/);
+  await expect(profileInfo).toHaveText(/Newleaf/);
+  await expect(profileInfo).toHaveText(/0 moons/);
+});
