ce14d89efe3190ee7ce8d8e75de0e73721c3da07
Lara <63805048+larkgz@users.noreply.github.com>
2025-12-01 18:13:08 -0500

    fix: better meter height on chrome

--- src/components/generic/ProgressBar.tsx
+++ src/components/generic/ProgressBar.tsx
@@ -6,7 +6,7 @@ function ProgressBar({ value }: { value: number }) {
           min={0} max={100}
           value={value}
           aria-valuenow={value}
-          style={{ width: "140px" }}
+          style={{ width: "140px", height: "1.5em" }}
         >
         </meter>
       </div>
