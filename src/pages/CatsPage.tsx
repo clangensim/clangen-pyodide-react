098260f61b56ae8789ab6783cecc1709c1952f0a
Lara <63805048+larkgz@users.noreply.github.com>
2025-12-15 18:12:44 -0500

    feat: cat page redesign

--- src/pages/CatsPage.tsx
+++ src/pages/CatsPage.tsx
@@ -2,11 +2,11 @@ import { clangenRunner } from "../python/clangenRunner";
 import { Link } from "react-router";
 import CatDisplay from "../components/CatDisplay";
 import { useEffect, useState } from "react";
-import Checkbox from "../components/generic/Checkbox";
 import BasePage from "../layout/BasePage";
 
 import "../styles/cats-page.css";
 import { Cat } from "../python/types";
+import { useQuery } from "@tanstack/react-query";
 
 const crumbs = [
   {
@@ -19,11 +19,27 @@ const crumbs = [
   },
 ];
 
+type ScreenState = "starclan" | "dark_forest" | "clan_cats" | "outside_cats" | "unknown_residence";
+
 function CatsPage() {
-  const [showLiving, setShowLiving] = useState(true);
-  const [showDead, setShowDead] = useState(false);
-  const [showOutside, setShowOutside] = useState(false);
   const [cats, setCats] = useState<Cat[]>([]);
+  const query = useQuery({
+    queryKey: ["claninfo"],
+    queryFn: async () => await clangenRunner.getClanInfo(),
+  });
+  const clanInfo = query.data;
+
+
+  let showLiving;
+  let showDead;
+  const [screenState, setScreenState] = useState<ScreenState>("clan_cats");
+  if (screenState === "clan_cats" || screenState === "outside_cats") {
+    showLiving = true;
+    showDead = false;
+  } else if (screenState === "starclan" || screenState === "dark_forest" || screenState === "unknown_residence") {
+    showLiving = false;
+    showDead = true;
+  }
 
   useEffect(() => {
     document.title = "Cats | ClanGen Simulator";
@@ -32,30 +48,40 @@ function CatsPage() {
 
   return (
     <BasePage crumbs={crumbs}>
-      <Checkbox
-        label="Show living"
-        checked={showLiving}
-        onChange={() => setShowLiving(!showLiving)}
-      />
-      <Checkbox
-        label="Show deceased"
-        checked={showDead}
-        onChange={() => setShowDead(!showDead)}
-      />
-      <Checkbox
-        label="Show cats outside the Clan"
-        checked={showOutside}
-        onChange={() => setShowOutside(!showOutside)}
-      />
+      <div className="cats-list__nav">
+        <div className="button-row" style={{marginLeft: "0.25em"}}>
+          { showLiving && 
+            <>
+              <button className="btn-secondary" onClick={() => setScreenState("clan_cats")}>{clanInfo ? clanInfo.name : "Clan"} Cats</button>
+              <button className="btn-secondary" onClick={() => setScreenState("outside_cats")}>Cats Outside the Clan</button>
+            </>
+          }
+          { showDead && 
+            <>
+              <button onClick={() => setScreenState("starclan")}>StarClan</button>
+              <button onClick={() => setScreenState("dark_forest")}>Dark Forest</button>
+              <button onClick={() => setScreenState("unknown_residence")}>???</button>
+            </>
+          }
+        </div>
+
+        <div>
+          {showDead && <button onClick={() => { setScreenState("clan_cats") }} className="link-button">Show living</button>}
+          {showLiving && <button onClick={() => { setScreenState("starclan") }} className="link-button">Show deceased</button>}
+        </div>
+      </div>
+
       <div className="cats-list">
         {cats.map((cat, index) => {
-          if (cat.dead && !showDead) {
+          if (screenState === "dark_forest" && (!cat.dead || !cat.inDarkForest )) {
             return;
-          }
-          if (!cat.dead && !showLiving) {
+          } else if (screenState === "starclan" && (!cat.dead || cat.inDarkForest || cat.outside)) {
             return;
-          }
-          if (cat.outside && !showOutside) {
+          } else if (screenState === "clan_cats" && (cat.dead || cat.outside)) {
+            return;
+          } else if (screenState === "outside_cats" && (cat.dead || !cat.outside)) {
+            return;
+          } else if (screenState === "unknown_residence" && (!cat.dead || cat.inDarkForest || !cat.outside)) {
             return;
           }
           return (
