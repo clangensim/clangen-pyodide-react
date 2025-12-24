6144439ed6e1e0539d178049d383120ed858ff58
Lara <63805048+larkgz@users.noreply.github.com>
2025-12-24 15:55:50 -0500

    chore: className property for checkbox label

--- src/components/generic/Checkbox.tsx
+++ src/components/generic/Checkbox.tsx
@@ -4,17 +4,19 @@ function Checkbox({
   label,
   checked,
   onChange,
+  className,
 }: {
   label: React.ReactNode;
   checked?: boolean;
   onChange?: () => void;
+  className?: string;
 }) {
   const ID = useId();
 
   return (
     <>
       <div className="checkbox-row">
-        <label>
+        <label className={className}>
           <input
             tabIndex={0}
             id={ID}
