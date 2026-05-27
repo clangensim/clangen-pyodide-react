235b2ecdc95e46b3d2b401b749bbe23832602c95
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-27 17:31:43 -0400

    feat: Symbol component

---
+++ src/components/Symbol.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import symbolsIndex from "../assets/symbolsIndex.json";
+
+import { loadImage } from "../utils";
+
+type Symbol = keyof typeof symbolsIndex;
+
+async function drawSymbol(symbol: Symbol, canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext("2d")!;
+
+  const symbolData = symbolsIndex[symbol];
+  const symbolImage = await loadImage("/sprites/symbols.png");
+
+  ctx.drawImage(
+    symbolImage,
+    symbolData.xOffset,
+    symbolData.yOffset,
+    50,
+    50,
+    0,
+    0,
+    50,
+    50,
+  );
+}
+
+function ClanSymbol({ symbol }: { symbol?: string }) {
+  console.log(symbol);
+  if (!symbol || !(symbol in symbolsIndex)) {
+    return <></>;
+  } // symbolName in symbolsIndex
+
+  const canvasElement = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasElement.current) {
+      return;
+    }
+    drawSymbol(symbol as Symbol, canvasElement.current);
+  }, [canvasElement]);
+
+  return <canvas width={50} height={50} ref={canvasElement}></canvas>;
+}
+
+export default ClanSymbol;
