4e6c25d509adc724f4d4b3a86d3a41f9b29ca6bb
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-30 22:45:38 -0400

    feat: tabs on leader's den

--- src/pages/dens/LeadersDenPage.tsx
+++ src/pages/dens/LeadersDenPage.tsx
@@ -6,6 +6,7 @@ import ClanSymbol from "../../components/Symbol";
 import Radiobox from "../../components/generic/Radiobox";
 import CatDisplay from "../../components/CatDisplay";
 import { Link } from "react-router";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 const crumbs = [
   {
@@ -41,85 +42,112 @@ function LeadersDenPage() {
 
   return (
     <BasePage crumbs={crumbs}>
-      <h2>Gatherings</h2>
-      <p className="below-header">
-        Determine how to treat another Clan at the next Gathering.
-      </p>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Clan</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {otherClans.map((oc) => (
-            <tr>
-              <td>
-                <ClanSymbol symbol={oc.symbol} />
-              </td>
-              <td>
-                <b>{oc.name}Clan</b> <br />
-                {oc.temperament} <br /> neutral
-              </td>
-              <td>
-                <Radiobox label="Offend" name="action" />{" "}
-                <Radiobox label="Praise" name="action" />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button className="link-button">Clear selection</button>
+      Manage relationships with other cats who live nearby.
+      <TabGroup vertical>
+        <TabList className="button-row">
+          <ul className="below-header row-list">
+            <li>
+              <Tab className="link-button">Gatherings</Tab>
+            </li>
+            <li>
+              <Tab className="link-button">Order a Search</Tab>
+            </li>
+          </ul>
+        </TabList>
 
-      <h2>Order a Search</h2>
-      <ul className="below-header">
-        <li>
-          Search for - Search for this lost cat to bring them home.
-        </li>
-        <li>Hunt down - Hunt down this cat and kill them.</li>
-        <li>
-          Invite in - Find this cat to invite them to join the Clan.
-        </li>
-        <li>Drive off - Drive this cat away if you find them.</li>
-      </ul>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Cat</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cats
-            .filter((c) => c.outside && !c.dead && !c.isDrivenOff)
-            .map((c) => (
-              <tr>
-                <td>
-                  <Link to={`/cats/${c.ID}`} >
-                  <CatDisplay cat={c} fuzzy={true} w="50px" h="50px" />
-                  </Link>
-                </td>
-                <td>
-                  <b>{c.name.display}</b> <br />
-                  {c.status} <br />
-                </td>
-                <td>
-                  {isLost(c) && (
-                    <Radiobox label="Search for" name="outsider-action" />
-                  )}
-                  <Radiobox label="Hunt down" name="outsider-action" />{" "}
-                  <Radiobox label="Invite in" name="outsider-action" />
-                  <Radiobox label="Drive off" name="outsider-action" />
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-      <button className="link-button">Clear selection</button>
-      <button className="submit">Submit</button>
+        <TabPanels>
+          <TabPanel>
+            <h2>Gatherings</h2>
+            <p className="below-header">
+              Determine how to treat another Clan at the next Gathering.
+            </p>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Clan</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {otherClans.map((oc) => (
+                  <tr>
+                    <td>
+                      <ClanSymbol symbol={oc.symbol} />
+                    </td>
+                    <td>
+                      <b>{oc.name}Clan</b> <br />
+                      {oc.temperament} <br /> neutral
+                    </td>
+                    <td>
+                      <Radiobox label="Offend" name="action" />{" "}
+                      <Radiobox label="Praise" name="action" />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button tabIndex={0} className="link-button">
+              Clear selection
+            </button>
+            <button tabIndex={0} className="submit">
+              Submit
+            </button>
+          </TabPanel>
+
+          <TabPanel>
+            <h2>Order a Search</h2>
+            <ul className="below-header">
+              <li>Search for - Search for this lost cat to bring them home.</li>
+              <li>Hunt down - Hunt down this cat and kill them.</li>
+              <li>
+                Invite in - Find this cat to invite them to join the Clan.
+              </li>
+              <li>Drive off - Drive this cat away if you find them.</li>
+            </ul>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Cat</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cats
+                  .filter((c) => c.outside && !c.dead && !c.isDrivenOff)
+                  .map((c) => (
+                    <tr>
+                      <td>
+                        <Link to={`/cats/${c.ID}`}>
+                          <CatDisplay cat={c} fuzzy={true} w="50px" h="50px" />
+                        </Link>
+                      </td>
+                      <td>
+                        <b>{c.name.display}</b> <br />
+                        {c.status} <br />
+                      </td>
+                      <td>
+                        {isLost(c) && (
+                          <Radiobox label="Search for" name="outsider-action" />
+                        )}
+                        <Radiobox label="Hunt down" name="outsider-action" />{" "}
+                        <Radiobox label="Invite in" name="outsider-action" />
+                        <Radiobox label="Drive off" name="outsider-action" />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+            <button tabIndex={0} className="link-button">
+              Clear selection
+            </button>
+            <button tabIndex={0} className="submit">
+              Submit
+            </button>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
     </BasePage>
   );
 }
